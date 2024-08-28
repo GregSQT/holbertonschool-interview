fd861d4643e08fef9992b297c483093e0cb46644
GregSQT <gregory.souquet@hotmail.fr>
2024-08-28 23:50:34 +0200

    Update 0-lockboxes.py

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -3,7 +3,7 @@
 """
 Exercice :
 You have n number of locked boxes in front of you.
-Each box is numbered sequentially from 0 to n - 1 
+Each box is numbered sequentially from 0 to n - 1
 and each box may contain keys to the other boxes.
 Write a method that determines if all the boxes can be opened.
 """
@@ -31,4 +31,3 @@ def canUnlockAll(boxes):
             # If no key has benn found
             return boxes_openable
     return True
-    
