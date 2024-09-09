df2ecfb2bb4c05fe10f3f73cb76dde7a22ac9fb5
GregSQT <gregory.souquet@hotmail.fr>
2024-09-09 12:29:53 +0200

    heap insert

--- heap_insert/binary_trees.h
+++ heap_insert/binary_trees.h
@@ -1,10 +1,6 @@
-#ifndef BINARY_TREES__H
-#define BINARY_TREES__H
-
-#include <stddef.h>
+#ifndef HEAP_H
+#define HEAP_H
 #include <stdlib.h>
-
-
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -20,13 +16,12 @@ struct binary_tree_s
 	struct binary_tree_s *left;
 	struct binary_tree_s *right;
 };
-
-typedef struct binary_tree_s binary_tree_t;
-typedef struct binary_tree_s heap_t;
-
+typedef struct binary_tree_s heap_t, binary_tree_t;
 void binary_tree_print(const binary_tree_t *);
-
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 heap_t *heap_insert(heap_t **root, int value);
-
-#endif /* BINARY_TREES__H */
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
+int height(binary_tree_t *tree);
+int binary_tree_is_perfect(const binary_tree_t *tree);
+#endif
