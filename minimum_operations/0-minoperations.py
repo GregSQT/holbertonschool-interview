027d72a62e4052cecba630f9d25730f86bc41023
GregSQT <gregory.souquet@hotmail.fr>
2024-09-16 11:49:09 +0200

    miniop

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -3,7 +3,8 @@
 In a text file, there is a single character H. Your text editor can execute
 only two operations in this file: Copy All and Paste. Given a number n,
 write a method that calculates the fewest number of operations needed to result
-in exactly n H characters in the file. Be smart about how you utilize the memory!
+in exactly n H characters in the file.
+Be smart about how you utilize the memory!
 """
 
 
