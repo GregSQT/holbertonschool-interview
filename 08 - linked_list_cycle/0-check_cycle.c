4f0e85203882454f1c415b330fbcb47211bc988e
GregSQT <9604@holbertonstudents.com>
2025-03-19 05:23:14 +0100

    linked list cycle

--- 08 - linked_list_cycle/0-check_cycle.c
+++ 08 - linked_list_cycle/0-check_cycle.c
@@ -1,28 +1,29 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 #include "lists.h"
 
 /**
- * check_cycle - check list
- * @list: list
- * Return: 1 or  0.
+ * check_cycle - checks if a singly linked list has a cycle in it.
+ * @list: pointer to the list to be checked
+ *
+ * Return: 0 if there is no cycle, 1 if there is a cycle
  */
 int check_cycle(listint_t *list)
 {
-	listint_t *lent0;
-	listint_t *lent1;
+	listint_t *fast, *slow;
 
-	if (list == NULL)
+	if (!list)
 		return (0);
-	lent0 = list;
-	lent1 = list;
+	slow = list;
+	fast = list->next;
 
-	while (lent0->next && lent1->next)
+	while (slow && fast && fast->next)
 	{
-		lent0 = lent0->next;
-		lent1 = lent1->next->next;
-		if (!lent1)
-			return (0);
-		if (lent0 == lent1)
+		if (fast->n == slow->n)
 			return (1);
+		fast = fast->next->next;
+		slow = slow->next;
 	}
 	return (0);
 }
