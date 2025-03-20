ae3fd883edd4350079cca183c56cf91d509d45bf
GregSQT <9604@holbertonstudents.com>
2025-03-20 02:51:38 +0100

    lp

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -1,36 +1,52 @@
 #!/usr/bin/python3
-'''line by line and computes metrics'''
+
+'''
+Reads stdin line by line and computes metrics
+'''
 import sys
 
+if __name__ == "__main__":
 
-f_size = 0
-status_codes = {
-    '200': 0, '301': 0, '400': 0, '401': 0,
-    '403': 0, '404': 0, '405': 0, '500': 0
-}
+    status_codes = {200: 0, 301: 0, 400: 0, 401: 0,
+                    403: 0, 404: 0, 405: 0, 500: 0}
+    file_size = [0]
+    count = 1
 
-try:
-    for i, line in enumerate(sys.stdin, 1):
-        infos_list = line.split()
-        if len(infos_list) > 2:
-            try:
-                f_size += int(infos_list[-1])
-            except Exception as e:
-                raise e
-            try:
-                if infos_list[-2] in status_codes:
-                    if isinstance(int(infos_list[-2]), int):
-                        status_codes[infos_list[-2]] += 1
-            except Exception as e:
-                raise e
-        if i % 10 == 0:
-            print('File size: {}'.format(f_size))
-            for k in sorted(status_codes):
-                if status_codes[k] != 0:
-                    print('{}: {}'.format(k, status_codes[k]))
-except KeyboardInterrupt:
-    pass
-print('File size: {}'.format(f_size))
-for k in sorted(status_codes):
-    if status_codes[k] != 0:
-        print('{}: {}'.format(k, status_codes[k]))
+    def print_stats():
+        '''
+        Prints file size and stats for every 10 loops
+        '''
+        print('File size: {}'.format(file_size[0]))
+
+        for code in sorted(status_codes.keys()):
+            if status_codes[code] != 0:
+                print('{}: {}'.format(code, status_codes[code]))
+
+    def parse_stdin(line):
+        '''
+        Checks the stdin for matches
+        '''
+        try:
+            line = line[:-1]
+            word = line.split(' ')
+            # File size is last parameter on stdout
+            file_size[0] += int(word[-1])
+            # Status code comes before file size
+            status_code = int(word[-2])
+            # Move through dictionary of status codes
+            if status_code in status_codes:
+                status_codes[status_code] += 1
+        except BaseException:
+            pass
+
+    try:
+        for line in sys.stdin:
+            parse_stdin(line)
+            # print the stats after every 10 outputs
+            if count % 10 == 0:
+                print_stats()
+            count += 1
+    except KeyboardInterrupt:
+        print_stats()
+        raise
+    print_stats()
