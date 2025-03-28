e43c1b698a41d92619d53da24841cd391c451a46
GregSQT <9604@holbertonstudents.com>
2025-03-28 11:02:10 +0100

    update

--- utf8_validation/0-validate_utf8.py
+++ utf8_validation/0-validate_utf8.py
@@ -1,40 +1,30 @@
 #!/usr/bin/python3
-"""Determines a valid UTF-8 encoding"""
+"""UTF-8 validation
+"""
 
 
 def validUTF8(data):
-    """
-    bit1 checks if significant byte is 1
-    bit2 checks if second significant byte is 0
-    nbytes = 1s before 0 occurs
-    data is a list of integers to check
-    """
+    """ validate utf-8 in data comming """
 
-    bit1 = 1 << 7
-    bit2 = 1 << 6
-    nbytes = 0
+    count = 0
 
-    if not data or len(data) == 0:
-        return True
-
-    for num in data:
-        bit = 1 << 7
-        if nbytes == 0:
-            while (bit & num):
-                nbytes += 1
-                bit = bit >> 1
-
-            if nbytes == 0:
-                continue
-            if nbytes == 1 or nbytes > 4:
+    for bit in data:
+        binary = bin(bit).replace('0b', '').rjust(8, '0')[-8:]
+        if count == 0:
+            if binary.startswith('110'):
+                count = 1
+            if binary.startswith('1110'):
+                count = 2
+            if binary.startswith('11110'):
+                count = 3
+            if binary.startswith('10'):
                 return False
         else:
-
-            if not (num & bit1 and not (num & bit2)):
+            if not binary.startswith('10'):
                 return False
-        nbytes -= 1
+            count -= 1
 
-    if nbytes:
+    if count != 0:
         return False
-    else:
-        return True
+
+    return True
