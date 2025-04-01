7b35f8eaeaaae9f4c50fe63a6552fc803254a050
GregSQT <9604@holbertonstudents.com>
2025-04-01 04:57:55 +0200

    heap sort

--- heap_sort/sort.h
+++ heap_sort/sort.h
@@ -1,9 +1,9 @@
-#ifndef SORT_H
-#define SORT_H
+#ifndef _SORT_H_
+#define _SORT_H_
 
-#include <stdlib.h>
+#include <stdio.h>
 
-void print_array(const int *array, size_t size);
 void heap_sort(int *array, size_t size);
+void print_array(const int *array, size_t size);
 
-#endif
+#endif /* _SORT_H_ */
