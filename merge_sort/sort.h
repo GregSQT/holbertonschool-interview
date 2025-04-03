5865fb4ff989a1e5fd1121ff3b935f49b0dc4ca5
GregSQT <9604@holbertonstudents.com>
2025-04-03 11:27:03 +0200

    merge sort

--- merge_sort/sort.h
+++ merge_sort/sort.h
@@ -1,7 +1,9 @@
-#ifndef _SORT_H
-#define _SORT_H
-#include <stdio.h>
-#include <stdlib.h>
+#ifndef SORT_H
+#define SORT_H
+
+#include <stddef.h>
+
 void print_array(const int *array, size_t size);
 void merge_sort(int *array, size_t size);
+
 #endif
